b2f87656c12617c0c17ef90ae2115f9cc140c2a3
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-06-18 14:16:09 +0100

    Switch to edamam api

--- src/service/recipes.js
+++ src/service/recipes.js
@@ -1,7 +1,9 @@
 import axios from "axios";
-const baseUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${
-  import.meta.env.VITE_API_KEY
-}`;
+const baseUrl = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${
+  import.meta.env.VITE_APP_ID
+}&app_key=${
+  import.meta.env.VITE_APP_KEY
+}&field=uri&field=image&field=url&field=label&field=ingredientLines&field=mealType&field=dishType&field=yield&field=calories&q=beef`;
 
 const getRecipes = async () => {
   const response = await axios.get(baseUrl);
