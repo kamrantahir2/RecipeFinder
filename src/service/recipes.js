8e2d233c357052293d31cb34d0d18038bcf84c31
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-06-19 21:46:46 +0100

    Return 100 results

--- src/service/recipes.js
+++ src/service/recipes.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // &to=100 Add this to baseUrl for 100 recipes
 const baseUrl = `https://api.edamam.com/search?app_id=${
   import.meta.env.VITE_APP_ID
-}&app_key=${import.meta.env.VITE_APP_KEY}`;
+}&app_key=${import.meta.env.VITE_APP_KEY}&to=100`;
 
 const getRecipes = async (query) => {
   const response = await axios.get(`${baseUrl}&q=${query}`);
