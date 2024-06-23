dc56d096f01e63c79787071fa7164f5114b4fc9a
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-06-23 20:54:37 +0100

    small styling change

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
   }
 
   return (
-    <div className="font-playwrite w-screen">
+    <div className="font-playwrite">
       <Hero />
       <form onSubmit={handleSubmit} className="Search mt-2 md:mt-0">
         <input
