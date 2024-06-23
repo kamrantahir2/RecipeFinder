e2ff30ac15f4f599a878e30f12ab555b4fa870c8
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-06-23 20:03:59 +0100

    Small styling change

--- src/App.jsx
+++ src/App.jsx
@@ -44,21 +44,21 @@ function App() {
               className=" card mx-2 bg-gray-50 capitalize text-black shadow-2xl my-7 "
               key={r.recipe.uri}
             >
-              <div className="font-se">
-                <h2 className="m-auto text-2xl mt-8 ">{r.recipe.label}</h2>
-                <h2 className="mb-3">{r.recipe.cuisineType}</h2>
+              <div className="">
+                <h2 className="m-auto lg:text-2xl mt-8 ">{r.recipe.label}</h2>
+                <h2 className="mb-3 text-sm mt-4">{r.recipe.cuisineType}</h2>
               </div>
               <figure className="">
                 <img
                   src={r.recipe.image}
-                  className=" w-10/12  lg:w-9/12 rounded-xl"
+                  className=" w-10/12 lg:w-9/12 rounded-xl"
                   alt={r.recipe.label}
                 />
               </figure>
 
               <div className=" mt-3 md:mt-0 md:card-body"></div>
               <a href={r.recipe.url} target="_blank">
-                <button className="btn btn-primary mb-6 text-white text-lg h-14 lg:w-9/12 ">
+                <button className="btn btn-primary mb-6 text-white lg:text-lg lg:h-14 lg:w-9/12 ">
                   Find out more
                 </button>
               </a>
