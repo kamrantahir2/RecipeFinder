200d3c10e198776d766b720339cfee91b1c390d5
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-06-25 21:00:45 +0100

    Small styling chnage to make card contents in line with other cards

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -2,11 +2,11 @@ const Recipe = ({ recipe }) => {
   return (
     <div className=" card mx-2 bg-gray-50 capitalize text-black shadow-2xl my-7 p-2 w-full lg:w-3/12 flex-grow">
       <div className="">
-        <h2 className="m-auto lg:text-2xl mt-8 ">{recipe.recipe.label}</h2>
+        <h2 className=" lg:text-2xl mt-8 ">{recipe.recipe.label}</h2>
       </div>
 
       <div className=" mt-3 md:mt-0 md:card-body">
-        <h2 className="mb-3 text-sm mt-4">{recipe.recipe.cuisineType}</h2>
+        <h2 className="mb-3 text-sm mt-auto">{recipe.recipe.cuisineType}</h2>
         <figure className="">
           <img
             src={recipe.recipe.image}
@@ -14,7 +14,7 @@ const Recipe = ({ recipe }) => {
             alt={recipe.recipe.label}
           />
         </figure>
-        <a href={recipe.recipe.url} target="_blank" className="mt-auto">
+        <a href={recipe.recipe.url} target="_blank" className="">
           <button className="btn btn-primary mt-4 mb-6 text-white lg:text-lg lg:h-14 lg:w-9/12 ">
             Find out more
           </button>
